a9249886b516e1f8bcf0f3d07f98a5f6a011f0ad
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-03-24 16:20:06 +0800

    init 4 different tabs, Home, Chat, Map, and Profile

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,17 @@
 <script lang="ts">
 	import '../app.css';
+	import { ModeWatcher } from "mode-watcher"
 	let { children } = $props();
 </script>
 
-{@render children()}
+<ModeWatcher />
+
+<div class="h-screen flex flex-col justify-between">
+	{@render children()}
+	<footer class="flex w-full justify-evenly pb-8">
+		<a href="/">Home</a>
+		<a href="/chat">Chat</a>
+		<a href="/map">Map</a>
+		<a href="/profile">Profile</a>
+	</footer>
+</div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+</script>
+
+<main class="w-full flex justify-center pt-10">
+    <h1 class="text-3xl">Brew It</h1>
+</main>

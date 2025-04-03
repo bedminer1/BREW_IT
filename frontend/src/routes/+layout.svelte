e9e4445b928f32c0f1d4bad1ebf449fafebce93c
bedminer1 <alexander.seah.min.rui@gmail.com>
2025-04-04 07:39:24 +0800

    fixed toolbar bg on lightmode

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -13,7 +13,7 @@
 	<main class="w-full flex flex-col p-4 justify-center items-start pb-24">
 		{@render children()}
 	</main>
-	<footer class="fixed bottom-0 left-0 flex w-full justify-evenly py-6 border-2 bg-black  opacity-100">
+	<footer class="fixed bottom-0 left-0 flex w-full justify-evenly py-6 border-2 bg-background  opacity-100">
 		<a href="/brewery" class={page.url.pathname.startsWith("/brewery") ? "opacity-100" : "opacity-55"}><Beer /></a>
 		<a href="/explore" class={page.url.pathname.startsWith("/explore") ? "opacity-100" : "opacity-55"}><Telescope /></a>
 		<a href="/map" class={page.url.pathname.startsWith("/map") ? "opacity-100" : "opacity-55"}><Map /></a>
--- frontend/src/routes/settings/+page.svelte
+++ frontend/src/routes/settings/+page.svelte
@@ -3,7 +3,6 @@
         import Button from "$lib/components/ui/button/button.svelte";
     </script>
     
-    Settings Placeholder
     <div>
         <Button onclick={toggleMode}>Switch mode</Button>
     </div>
